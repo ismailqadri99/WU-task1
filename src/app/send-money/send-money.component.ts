f3e041434d4d1291c975d0df47db5bd12d344b10
Mohammad Ismail Qadri <mohammadismail.qadri@hcl.com>
2023-11-21 21:11:13 +0530

    Almost completed

---
+++ src/app/send-money/send-money.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-send-money',
+  templateUrl: './send-money.component.html',
+  styleUrls: ['./send-money.component.css']
+})
+export class SendMoneyComponent implements OnInit {
+  countries:any [];
+SendMoney() {
+
+}
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
